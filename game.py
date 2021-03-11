9e48222ac00522a1c56776eddfd5b79f2c40dff2
rauan_sanfelice <rauan.sanfelice@gmail.com>
2021-03-11 14:04:13 -0300

    🚧 Criando logica de preeenchimento

--- game.py
+++ game.py
@@ -5,7 +5,9 @@ from pygame.locals import *
 pygame.init()
 FONT = pygame.font.Font(None, 42)
 
-DIFFICULTY_GAME = [('Fácil', 0), ('Normal', 1), ('Difícil', 2)]
+DIFFICULTY_GAME_TEXT = [('Fácil', 0), ('Normal', 1), ('Difícil', 2)]
+DIFFICULTY_GAME_WEIGHTS = [[50, 50], [60, 40], [70, 30]]
+DIFFICULTY_GAME = DIFFICULTY_GAME_WEIGHTS[0]
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 596
 
@@ -29,28 +31,239 @@ BIG_SQUARE_SIZE = (SMALL_SQUARE_SIZE * 3) + SIZE_DIVISION_BIG_SQUARE
 ERROS = 0
 NUM_FALTANTES = 81
 NUM_PREENCHIDOS = 0
-GRID = [
-    [8, 7, 9, 6, 5, 1, 3, 2, 4],
-    [5, 2, 3, 7, 4, 9, 1, 8, 6],
-    [1, 6, 4, 2, 3, 8, 7, 9, 5],
+# GRID = [
+#     [8, 7, 9, 6, 5, 1, 3, 2, 4],
+#     [5, 2, 3, 7, 4, 9, 1, 8, 6],
+#     [1, 6, 4, 2, 3, 8, 7, 9, 5],
     
-    [6, 9, 5, 1, 2, 7, 8, 4, 3],
-    [3, 1, 7, 8, 9, 4, 6, 5, 2],
-    [2, 4, 8, 5, 6, 3, 9, 1, 7],
+#     [6, 9, 5, 1, 2, 7, 8, 4, 3],
+#     [3, 1, 7, 8, 9, 4, 6, 5, 2],
+#     [2, 4, 8, 5, 6, 3, 9, 1, 7],
     
-    [4, 3, 1, 9, 7, 5, 2, 6, 8],
-    [9, 5, 6, 3, 8, 2, 4, 7, 1],
-    [7, 8, 2, 4, 1, 6, 5, 3, 9]
-]
+#     [4, 3, 1, 9, 7, 5, 2, 6, 8],
+#     [9, 5, 6, 3, 8, 2, 4, 7, 1],
+#     [7, 8, 2, 4, 1, 6, 5, 3, 9]
+# ]
+
+        
+
+class Grid:
+
+    def __init__(self):
+        self.grid = []
+    
+    def initialize(self):
+        
+        sequencia = [
+            { "col": 8, "row": None},
+            { "col": None, "row": 8},
+            { "col": 0, "row": None},
+            { "col": None, "row": 1},
+            { "col": 7, "row": None},
+            { "col": None, "row": 7},
+            { "col": 1, "row": None},
+            { "col": None, "row": 2},
+            { "col": 6, "row": None},
+            { "col": None, "row": 6},
+            { "col": 2, "row": None},
+            { "col": None, "row": 3},
+            { "col": 5, "row": None},
+            { "col": None, "row": 5},
+            { "col": 3, "row": None},
+        ]
+
+        list_int = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        random.shuffle(list_int)
+        qdr = -1 
+        for i, item in enumerate(list_int):
+            if i % 3 == 0:
+                qdr += 1
+            self.grid.append({
+                "text": item,
+                "row": 0,
+                "column": i,
+                "quadrante": qdr,
+                "preeenchido": False,
+                "error": False,
+            })
+        
+        get_text = lambda x: x['text']
+        get_column = lambda x: x['column']
+        filter_qdr = lambda x: x['quadrante'] == qdr
+        filter_row = lambda x: x['row'] == row
+        filter_col = lambda x: x['column'] == col
+        
+        row_qdr = 0
+        firts = True
+        
+        for item in sequencia:
+            
+            # CENARIO DE COLUNA
+            if item['col'] is not None:
+                
+                # REMOVE NUMEROS QUE JA POSSUI NA COLUNA
+                col = item['col']
+                list_nums_in_col = list(filter(filter_col, self.grid))
+                nums_in_col = list(map(get_text, list_nums_in_col))
+                nums_faltantes = list(set([1, 2, 3, 4, 5, 6, 7, 8, 9]) - set(nums_in_col))
+                total_nums_faltantes = len(nums_faltantes)
+
+                for i in range(0, total_nums_faltantes, 1):
+
+                    # ATUALIZA E REMOVE NUMEROS QUE JA POSSUI NA COLUNA
+                    list_nums_in_col = list(filter(filter_col, self.grid))
+                    nums_in_col = list(map(get_text, list_nums_in_col))
+                    nums_faltantes = list(set([1, 2, 3, 4, 5, 6, 7, 8, 9]) - set(nums_in_col))
+
+                    # REMOVE NUMEROS QUE JA POSSUI NA LINHA
+                    row = max(list_nums_in_col, key=lambda x: x["row"])["row"] + 1
+                    list_nums_in_row = list(filter(filter_row, self.grid))
+                    nums_in_row = list(map(get_text, list_nums_in_row))
+                    nums_faltantes = list(set(nums_faltantes) - set(nums_in_row))
+                    
+                    # DESCOBRE QUADRANTE
+                    row_qdr  = int(row / 3)
+                    qdr  = int(col / 3) + (row_qdr * 3)
+
+                    # REMOVE NUMEROS QUE JA POSSUI NO QUADRANTE
+                    list_nums_in_qdr = list(filter(filter_qdr, self.grid))
+                    nums_in_qdr = list(map(get_text, list_nums_in_qdr))
+                    nums_faltantes = list(set(nums_faltantes) - set(nums_in_qdr))
+
+                    self.grid.append({
+                        "text": random.choices(nums_faltantes)[0],
+                        "row": row,
+                        "column": col,
+                        "quadrante": qdr,
+                        "preeenchido": False,
+                        "error": False,
+                    })
+
+            elif item['row'] is not None:
+                
+                # REMOVE NUMEROS QUE JA POSSUI NA LINHA
+                row = item['row']
+                list_nums_in_row = list(filter(filter_row, self.grid))
+                nums_in_row = list(map(get_text, list_nums_in_row))
+                nums_faltantes = list(set([1, 2, 3, 4, 5, 6, 7, 8, 9]) - set(nums_in_row))
+                total_nums_faltantes = len(nums_faltantes)
+
+                for i in range(0, total_nums_faltantes, 1):
+                    
+                    list_nums_in_row = list(filter(filter_row, self.grid))
+                    nums_in_row = list(map(get_text, list_nums_in_row))
+                    nums_faltantes = list(set([1, 2, 3, 4, 5, 6, 7, 8, 9]) - set(nums_in_row))
+
+                    col = min(list_nums_in_row, key=lambda x: x["column"])["column"] - 1
+
+                    # REMOVE NUMEROS QUE JA POSSUI NA COLUNA
+                    list_nums_in_col = list(filter(filter_col, self.grid))
+                    nums_in_col = list(map(get_text, list_nums_in_col))
+                    nums_faltantes = list(set(nums_faltantes) - set(nums_in_col))
+
+                    # DESCOBRE QUADRANTE
+                    row_qdr  = int(row / 3)
+                    qdr  = int(col / 3) + (row_qdr * 3)
+
+                    # REMOVE NUMEROS QUE JA POSSUI NO QUADRANTE
+                    list_nums_in_qdr = list(filter(filter_qdr, self.grid))
+                    nums_in_qdr = list(map(get_text, list_nums_in_qdr))
+                    nums_faltantes = list(set(nums_faltantes) - set(nums_in_qdr))
+
+                    self.grid.append({
+                        "text": random.choices(nums_faltantes)[0],
+                        "row": row,
+                        "column": col,
+                        "quadrante": qdr,
+                        "preeenchido": False,
+                        "error": False,
+                    })
+
+                    print("")
+                    print("")
+                    print("")
+                    self.print_grid()
+
+            print("")
+            print("")
+            print("")
+            self.order_grid()
+            self.print_grid()
+
+        return self.grid
+
+    def num_available_col(self, num:int, col:int):
+        
+        filter_col = lambda x: x['text'] == num
+        map_nums_col = lambda x: x['column']
+        
+        nums_col = list(filter(filter_col, self.grid))
+        nums_text_col = list(map(map_nums_col, nums_col))
+        
+        if col in nums_text_col:
+            return False
+        return True
+    
+    def search_available_qdrs(self, num):
+        
+        filter_qdr = lambda x: x['quadrante'] == index_qdr
+        map_num_qdr = lambda x: x['text']
+        result = []
+
+        for index_qdr in range(0, 3, 1):
+
+            nums_qdr = list(filter(filter_qdr, self.grid))
+            nums_text_qdr = list(map(map_num_qdr, nums_qdr))
+
+            if num not in nums_text_qdr:
+                result.append(index_qdr)
+
+        return result
+
+    def order_grid(self):
+        self.grid = sorted(self.grid, key=lambda x: (x["row"], x["column"]))
+
+    def print_grid(self):
+    
+        self.order_grid()
+        allGrid = []
+        find_elem = lambda x: x['row'] == row and x['column'] == col
+        get_text = lambda x: x["text"]
+
+        for row in range(0, 9, 1):
+            rowGrid = []
+            if row % 3 == 0:
+                allGrid.append(" ----------------------")
+
+            for col in range(0, 9, 1):
+                
+                if col == 0:
+                    rowGrid.append("|")
+
+                dict_num = list(filter(find_elem, self.grid))
+                if dict_num:
+                    number = str(list(map(get_text, dict_num))[0])
+                else:
+                    number = " "
+                
+                if col % 3 == 0 and col != 0:
+                    rowGrid.append("-|")
+                rowGrid.append(number)
+                
+                rowGrid.append("|")
+            allGrid.append(rowGrid)
+        
+        allGrid.append(" ----------------------")
+        for i in allGrid:
+            print("".join(i))
 
 class InputBox:
 
-
     def __init__(self, x:float, y:float, w:float, h:float, position:list, text:str=''):
-        global NUM_FALTANTES, NUM_PREENCHIDOS, ERROS
+        global DIFFICULTY_GAME, NUM_FALTANTES, NUM_PREENCHIDOS, ERROS
         
         if text == "":
-            if random.choices([0, 1], weights = [10, 90], k=1)[0] == 1:
+            if random.choices([0, 1], weights=DIFFICULTY_GAME, k=1)[0] == 1:
                 text = str(GRID[position[0]][position[1]])
                 NUM_FALTANTES -= 1
         
@@ -66,7 +279,8 @@ class InputBox:
         if event.type == pygame.MOUSEBUTTONDOWN:
             # If the user clicked on the input_box rect.
             if self.rect.collidepoint(event.pos):
-                self.active = not self.active
+                if self.text == "" or self.error:
+                    self.active = not self.active
             else:
                 self.active = False
             # Change the current color of the input box.
@@ -102,11 +316,128 @@ class InputBox:
             self.color = COLOR_ACTIVE
             self.error = False
 
+    def analyze(self, screen):
+        if self.active:
+            height = SCREEN_GAME_HEIGHT + 20
 
+            num_row = GRID[self.position[0]]
+            num_col = []
+            num_qdr = []
+            for row in GRID:
+                for index, col in enumerate(row):
+                    if index == self.position[1]:
+                        num_col.append(col)
+            
+            if self.position[0] <= 2:
+                if self.position[1] <= 2:
+                    num_qdr.append(GRID[0][0])
+                    num_qdr.append(GRID[0][1])
+                    num_qdr.append(GRID[0][2])
+                    num_qdr.append(GRID[1][0])
+                    num_qdr.append(GRID[1][1])
+                    num_qdr.append(GRID[1][2])
+                    num_qdr.append(GRID[2][0])
+                    num_qdr.append(GRID[2][1])
+                    num_qdr.append(GRID[2][2])
+                elif self.position[1] > 2 and self.position[1] <= 5:
+                    num_qdr.append(GRID[1][3])
+                    num_qdr.append(GRID[0][4])
+                    num_qdr.append(GRID[0][5])
+                    num_qdr.append(GRID[2][3])
+                    num_qdr.append(GRID[1][4])
+                    num_qdr.append(GRID[1][5])
+                    num_qdr.append(GRID[3][3])
+                    num_qdr.append(GRID[2][4])
+                    num_qdr.append(GRID[2][5])
+                else:
+                    num_qdr.append(GRID[1][6])
+                    num_qdr.append(GRID[0][7])
+                    num_qdr.append(GRID[0][8])
+                    num_qdr.append(GRID[2][6])
+                    num_qdr.append(GRID[1][7])
+                    num_qdr.append(GRID[1][8])
+                    num_qdr.append(GRID[3][6])
+                    num_qdr.append(GRID[2][7])
+                    num_qdr.append(GRID[2][8])
+            elif self.position[0] > 2 and self.position[0] <= 5:
+                if self.position[1] <= 2:
+                    num_qdr.append(GRID[3][0])
+                    num_qdr.append(GRID[3][1])
+                    num_qdr.append(GRID[3][2])
+                    num_qdr.append(GRID[4][0])
+                    num_qdr.append(GRID[4][1])
+                    num_qdr.append(GRID[4][2])
+                    num_qdr.append(GRID[5][0])
+                    num_qdr.append(GRID[5][1])
+                    num_qdr.append(GRID[5][2])
+                elif self.position[1] > 2 and self.position[1] <= 5:
+                    num_qdr.append(GRID[3][3])
+                    num_qdr.append(GRID[3][4])
+                    num_qdr.append(GRID[3][5])
+                    num_qdr.append(GRID[4][3])
+                    num_qdr.append(GRID[4][4])
+                    num_qdr.append(GRID[4][5])
+                    num_qdr.append(GRID[5][3])
+                    num_qdr.append(GRID[5][4])
+                    num_qdr.append(GRID[5][5])
+                else:
+                    num_qdr.append(GRID[3][6])
+                    num_qdr.append(GRID[3][7])
+                    num_qdr.append(GRID[3][8])
+                    num_qdr.append(GRID[4][6])
+                    num_qdr.append(GRID[4][7])
+                    num_qdr.append(GRID[4][8])
+                    num_qdr.append(GRID[5][6])
+                    num_qdr.append(GRID[5][7])
+                    num_qdr.append(GRID[5][8])
+            else:
+                if self.position[1] <= 2:
+                    num_qdr.append(GRID[6][0])
+                    num_qdr.append(GRID[6][1])
+                    num_qdr.append(GRID[6][2])
+                    num_qdr.append(GRID[7][0])
+                    num_qdr.append(GRID[7][1])
+                    num_qdr.append(GRID[7][2])
+                    num_qdr.append(GRID[8][0])
+                    num_qdr.append(GRID[8][1])
+                    num_qdr.append(GRID[8][2])
+                elif self.position[1] > 2 and self.position[1] <= 5:
+                    num_qdr.append(GRID[6][3])
+                    num_qdr.append(GRID[6][4])
+                    num_qdr.append(GRID[6][5])
+                    num_qdr.append(GRID[7][3])
+                    num_qdr.append(GRID[7][4])
+                    num_qdr.append(GRID[7][5])
+                    num_qdr.append(GRID[8][3])
+                    num_qdr.append(GRID[8][4])
+                    num_qdr.append(GRID[8][5])
+                else:
+                    num_qdr.append(GRID[6][6])
+                    num_qdr.append(GRID[6][7])
+                    num_qdr.append(GRID[6][8])
+                    num_qdr.append(GRID[7][6])
+                    num_qdr.append(GRID[7][7])
+                    num_qdr.append(GRID[7][8])
+                    num_qdr.append(GRID[8][6])
+                    num_qdr.append(GRID[8][7])
+                    num_qdr.append(GRID[8][8])
+
+            probabilidade = [1,2,3,4,5,6,7,8,9]
+            screen.blit(FONT.render(f'1: {probabilidade[0]}', True, COLOR_BLACK), (height, 110))
+            screen.blit(FONT.render(f'2: {probabilidade[1]}', True, COLOR_BLACK), (height, 140))
+            screen.blit(FONT.render(f'3: {probabilidade[2]}', True, COLOR_BLACK), (height, 170))
+            screen.blit(FONT.render(f'4: {probabilidade[3]}', True, COLOR_BLACK), (height, 200))
+            screen.blit(FONT.render(f'5: {probabilidade[4]}', True, COLOR_BLACK), (height, 230))
+            screen.blit(FONT.render(f'6: {probabilidade[5]}', True, COLOR_BLACK), (height, 260))
+            screen.blit(FONT.render(f'7: {probabilidade[6]}', True, COLOR_BLACK), (height, 290))
+            screen.blit(FONT.render(f'8: {probabilidade[7]}', True, COLOR_BLACK), (height, 320))
+            screen.blit(FONT.render(f'9: {probabilidade[8]}', True, COLOR_BLACK), (height, 350))
 
 # DIFICULDADE DO JOGO
 def set_difficulty(value, difficulty):
-    print('Dificuldade: ', DIFFICULTY_GAME[difficulty][0])
+    global DIFFICULTY_GAME
+    DIFFICULTY_GAME = DIFFICULTY_GAME_WEIGHTS[difficulty]
+    print('Dificuldade: ', DIFFICULTY_GAME_TEXT[difficulty][0])
 
 
 def draw_scenario():
@@ -264,6 +595,7 @@ def start_the_game():
         screen.fill((30, 30, 30))
         for box in input_boxes:
             box.draw(screen)
+            box.analyze(screen)
 
         pygame.display.flip()
         
@@ -277,12 +609,14 @@ def start_the_game():
             break
 
 
+GRID = Grid().initialize()
+
 # PROPIEDADES SCREEN
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # MENU
 menu = pygame_menu.Menu(SCREEN_HEIGHT, SCREEN_WIDTH, 'Bem Vindo(a)!', theme=pygame_menu.themes.THEME_GREEN)
-menu.add_selector('Dificuldade :', DIFFICULTY_GAME, onchange=set_difficulty)
+menu.add_selector('Dificuldade :', DIFFICULTY_GAME_TEXT, onchange=set_difficulty)
 menu.add_button('Jogar', start_the_game)
 menu.add_button('Sair', pygame_menu.events.EXIT)
 menu.mainloop(screen)
